edf6441da74b2aaed7cd802df4b4ce3afeadc5ef
S NAZEER BASHA <113363065+Nazeer-appdev4ever@users.noreply.github.com>
2022-09-12 21:41:31 +0530

    Update streamlit_app.py
    added header

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,5 @@ streamlit.text('Kale,Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.text('Avocado Toast')
+
+streamlit.header('Build Your Own Fruit Smoothie')
